08592de802986344a73dbf5e1b9f6fca356bf6a1
wz <2517577636@qq.com>
2024-06-14 10:55:06 +0800

    reactivity api:toRaw

--- src/mock/vue.core.mock.ts
+++ src/mock/vue.core.mock.ts
@@ -1,17 +1,28 @@
-import { reactive, isReactive } from "vue";
+import { reactive, isReactive, toRaw } from "vue";
 
-const original: any = { foo: 1 }
-const original2 = { bar: 2 }
-const original3 = { gg: 3 }
+/* 
+const original: any = { foo: 1 };
+const original2 = { bar: 2 };
+const original3 = { gg: 3 };
 
-const observed = reactive(original)
-const observed2 = reactive(original2)
-observed.bar = observed2
-observed.gg = original3
+const observed = reactive(original);
+const observed2 = reactive(original2);
+observed.bar = observed2;
+observed.gg = original3;
 
 console.log("observed: ", observed);
 console.log("observed2: ", observed2);
-console.log("observed.gg is reactive? ", isReactive(observed.gg));
+console.log("observed.gg is reactive? ", isReactive(observed.gg)); 
+*/
 
+/**
+ * @description toRaw on object using reactive as prototype
+ * */
+// debugger;
+const original = { foo: 1 };
+const observed = reactive(original);
+const inherted = Object.create(observed);
+const raw = toRaw(inherted);
 
-
+console.log("inherted: ", inherted);
+console.log("toRaw(inherted): ", toRaw(inherted));
